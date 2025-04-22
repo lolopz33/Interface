05e40f3130567a875d2cc480abb93acab3ff2932
lolopz33 <Lolsupz470@gmail.com>
2025-04-22 11:13:28 -0300

    Update and rename 2.lua to 2,1.lua

--- Ui/2,1.lua
+++ Ui/2,1.lua
@@ -587,8 +587,8 @@ function OrionLib:MakeWindow(WindowConfig)
 	}), {
 		AddThemeObject(Create("UIStroke", {
 			Color = Color3.fromRGB(200, 0, 255),
-			Thickness = 2,
-			Transparency = 0.1
+			Thickness = 3,
+			Transparency = 0.7
 		}), "Stroke"),
 	
 		SetChildren(SetProps(MakeElement("TFrame"), {
