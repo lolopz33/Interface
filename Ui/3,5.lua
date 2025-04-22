e4ff11b8793006c75e7952b1c70e5f32f7ee16c1
lolopz33 <Lolsupz470@gmail.com>
2025-04-22 15:24:38 -0300

    Update 3,6.lua

--- Ui/3,5.lua
+++ Ui/3,5.lua
@@ -14,12 +14,12 @@ local OrionLib = {
 	Flags = {},
 	Themes = {
 		Default = {
-			Main = Color3.fromRGB(25, 25, 25),
-			Second = Color3.fromRGB(32, 32, 32),
-			Stroke = Color3.fromRGB(60, 60, 60),
-			Divider = Color3.fromRGB(60, 60, 60),
-			Text = Color3.fromRGB(240, 240, 240),
-			TextDark = Color3.fromRGB(150, 150, 150)
+		    Main = Color3.fromRGB(28, 0, 60), -- Roxo escuro intenso
+		    Second = Color3.fromRGB(50, 0, 100), -- Roxo profundo
+		    Stroke = Color3.fromRGB(150, 0, 255), -- Roxo vibrante (neon)
+		    Divider = Color3.fromRGB(90, 0, 160), -- Roxo suave com um toque de azul
+		    Text = Color3.fromRGB(255, 255, 255), -- Branco puro para destaque
+		    TextDark = Color3.fromRGB(200, 180, 255) -- Roxo claro suave
 		}
 	},
 	SelectedTheme = "Default",
@@ -626,7 +626,7 @@ function OrionLib:MakeWindow(WindowConfig)
 		WindowStuff
 	}), "Main")
 
-	
+
 	if WindowConfig.ShowIcon then
 		WindowName.Position = UDim2.new(0, 50, 0, -24)
 		local WindowIcon = SetProps(MakeElement("Image", WindowConfig.Icon), {
