1d585f85de7f05828d7b9b98628776256356f08c
lolopz33 <Lolsupz470@gmail.com>
2025-04-22 00:43:37 -0300

    Update Interface.lua

--- Interface.lua
+++ Interface.lua
@@ -597,6 +597,15 @@ function OrionLib:MakeWindow(WindowConfig)
 		Size = UDim2.new(0, 615, 0, 344),
 		ClipsDescendants = true
 	}), {
+		SetProps(MakeElement("Image", "rbxassetid://3523728077"), {
+			AnchorPoint = Vector2.new(0.5, 0.5),
+			Position = UDim2.new(0.5, 0, 0.5, 0),
+			Size = UDim2.new(1, 80, 1, 320),
+			ImageColor3 = Color3.fromRGB(33, 33, 33),
+			ImageTransparency = 0.7
+		}),
+	}))
+	
 		--SetProps(MakeElement("Image", "rbxassetid://3523728077"), {
 		--	AnchorPoint = Vector2.new(0.5, 0.5),
 		--	Position = UDim2.new(0.5, 0, 0.5, 0),
@@ -604,6 +613,8 @@ function OrionLib:MakeWindow(WindowConfig)
 		--	ImageColor3 = Color3.fromRGB(33, 33, 33),
 		--	ImageTransparency = 0.7
 		--}),
+
+
 		SetChildren(SetProps(MakeElement("TFrame"), {
 			Size = UDim2.new(1, 0, 0, 50),
 			Name = "TopBar"
