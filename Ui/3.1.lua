793e909cb99dbc7844fe4784401c2a226f512068
lolopz33 <Lolsupz470@gmail.com>
2025-05-02 17:12:04 -0300

    Update and rename 3.0.lua to 3.1.lua

--- Ui/3.1.lua
+++ Ui/3.1.lua
@@ -605,12 +605,13 @@ function OrionLib:MakeWindow(WindowConfig)
 		Size = UDim2.new(0, 615, 0, 344),
 		ClipsDescendants = true
 	}), {
-		AddThemeObject(Create("UIStroke", {
-			Color = Color3.fromRGB(200, 0, 255),
-			Thickness = 4,
-			Transparency = 0.2
-		}), "Stroke"),
-	
+		--SetProps(MakeElement("Image", "rbxassetid://3523728077"), {
+		--	AnchorPoint = Vector2.new(0.5, 0.5),
+		--	Position = UDim2.new(0.5, 0, 0.5, 0),
+		--	Size = UDim2.new(1, 80, 1, 320),
+		--	ImageColor3 = Color3.fromRGB(33, 33, 33),
+		--	ImageTransparency = 0.7
+		--}),
 		SetChildren(SetProps(MakeElement("TFrame"), {
 			Size = UDim2.new(1, 0, 0, 50),
 			Name = "TopBar"
@@ -625,16 +626,15 @@ function OrionLib:MakeWindow(WindowConfig)
 				AddThemeObject(SetProps(MakeElement("Frame"), {
 					Size = UDim2.new(0, 1, 1, 0),
 					Position = UDim2.new(0.5, 0, 0, 0)
-				}), "Stroke"),
+				}), "Stroke"), 
 				CloseBtn,
 				MinimizeBtn
-			}), "Second"),
+			}), "Second"), 
 		}),
 		DragPoint,
 		WindowStuff
 	}), "Main")
 
-	
 	if WindowConfig.ShowIcon then
 		WindowName.Position = UDim2.new(0, 50, 0, -24)
 		local WindowIcon = SetProps(MakeElement("Image", WindowConfig.Icon), {
